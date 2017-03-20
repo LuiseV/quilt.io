a13069596bfb07dc058e2a7cefefad8703e422d3
Luise Valentin <valentin.luise@gmail.com>
2017-03-19 19:15:19 -0700

    Larger icons on hover

--- src/contact.php
+++ src/contact.php
@@ -22,29 +22,35 @@
 
         <div class="row">
           <div class="details">
-            <div class="col-xs-12 col-sm-4 info contact text-center">
-	      <a href=mailto:ethan@quilt.io>
-	        <i class="fa fa-slack big-icon" aria-hidden="true"></i>
-	        <h3>Request invite for Slack</h3>
-	      </a>
+            <div class="col-xs-12 col-sm-4 info text-center">
+	      <div class="contact">
+	        <a href=mailto:ethan@quilt.io>
+	          <i class="fa fa-slack big-icon" aria-hidden="true"></i>
+	          <h3>Request invite for Slack</h3>
+	        </a>
+	      </div>
             </div>
 
-            <div class="col-xs-12 col-sm-4 info contact text-center">
-	      <a href=mailto:ethan@quilt.io>
-	        <i class="fa fa-envelope-o big-icon" aria-hidden="true"></i>
-	        <h3>Email Ethan, the lead designer of Quilt</h3>
-              </a>
+            <div class="col-xs-12 col-sm-4 info text-center">
+	      <div class="contact">
+	        <a href=mailto:ethan@quilt.io>
+	          <i class="fa fa-envelope-o big-icon" aria-hidden="true"></i>
+	          <h3>Email Ethan, the lead designer of Quilt</h3>
+                </a>
+	      </div>
             </div>
 
-            <div class="col-xs-12 col-sm-4 info contact text-center">
-	      <i class="fa fa-users big-icon" aria-hidden="true"></i>
-	      <h3>Join the mailing lists</h3>
-	      <a href="https://groups.google.com/a/quilt.io/forum/#!forum/dev/join">
-		dev@quilt.io
-	      </a><br/>
-	      <a href="https://groups.google.com/a/quilt.io/forum/#!forum/discuss/join">
-		discuss@quilt.io
-              </a>
+            <div class="col-xs-12 col-sm-4 info text-center">
+	      <div class="contact">
+	        <i class="fa fa-users big-icon" aria-hidden="true"></i>
+	        <h3>Join the mailing lists</h3>
+	        <a href="https://groups.google.com/a/quilt.io/forum/#!forum/dev/join">
+		  dev@quilt.io
+	        </a><br/>
+	        <a href="https://groups.google.com/a/quilt.io/forum/#!forum/discuss/join">
+		  discuss@quilt.io
+                </a>
+	      </div>
             </div>
 
           </div>
